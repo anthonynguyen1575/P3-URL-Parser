d58fa90a955ae6d07a0de968ba09c7f5b6b8b391
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-11-15 19:56:35 -0800

    Add files via upload

---
+++ P3-URL-Parser.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::cout << "Please enter a URL: ";
+
+    std::string URL;
+    std::cin >> URL;
+
+    std::string scheme;
+    std::string authority;
+    std::string path;
+
+    int colonPlacement = URL.find(":");
+
+    scheme = URL.substr(0, colonPlacement + 1);
+
+    int forwardSlashPlacement = URL.find_first_of("/", colonPlacement + 3);
+
+    std::cout << forwardSlashPlacement << "\n";
+
+    authority = URL.substr(colonPlacement + 1, forwardSlashPlacement - colonPlacement - 1);
+
+    path = URL.substr(forwardSlashPlacement);
+    
+
+    std::cout << "scheme = " << scheme << "\n";
+    std::cout << "authority = " << authority << "\n";
+    std::cout << "path = " << path << "\n";
+}
